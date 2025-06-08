5ce9cdfc569b5e3555e4f35075660d35933ff5e4
bmkamal <55796617+bmkamal@users.noreply.github.com>
2025-06-08 17:43:09 +0200

    Added Transaction Entity Model properties and Lombok annotations

--- demo/src/main/java/com/eaglebank/demo/model/Transaction.java
+++ demo/src/main/java/com/eaglebank/demo/model/Transaction.java
@@ -1,4 +1,29 @@
 package com.eaglebank.demo.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "bankAccount")
+@EqualsAndHashCode(exclude = "bankAccount")
+@Entity
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private BigDecimal amount;
+    private String type;
+    private String currency;
+    private String reference;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_number")
+    private BankAccount bankAccount;
+    private OffsetDateTime createdTimestamp;
 }
