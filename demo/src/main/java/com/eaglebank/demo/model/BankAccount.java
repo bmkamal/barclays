8166d9a78879bc003ed0d6627f964fbe02f3bb91
bmkamal <55796617+bmkamal@users.noreply.github.com>
2025-06-08 17:32:24 +0200

    Added BankAccount Entity model properties as per Open API Specs

--- demo/src/main/java/com/eaglebank/demo/model/BankAccount.java
+++ demo/src/main/java/com/eaglebank/demo/model/BankAccount.java
@@ -1,4 +1,32 @@
 package com.eaglebank.demo.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = {"user", "transactions"})
+@EqualsAndHashCode(exclude = {"user", "transactions"})
+@Entity
 public class BankAccount {
+    @Id
+    private String accountNumber;
+    private String name;
+    private String accountType;
+    private BigDecimal balance;
+    private String currency;
+    private String sortCode;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id") private User user;
+    @OneToMany(mappedBy = "bankAccount", cascade = CascadeType.ALL)
+    private List<Transaction> transactions;
+    private OffsetDateTime createdTimestamp;
+    private OffsetDateTime updatedTimestamp;
 }
