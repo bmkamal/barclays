67e90813ea9a4ccbd31c90624197e6c8ac7c9190
bmkamal <55796617+bmkamal@users.noreply.github.com>
2025-06-08 17:19:31 +0200

    Added Address class

---
+++ demo/src/main/java/com/eaglebank/demo/model/Address.java
@@ -0,0 +1,10 @@
+package com.eaglebank.demo.model;
+
+public class Address {
+    private String line1;
+    private String line2;
+    private String line3;
+    private String town;
+    private String county;
+    private String postcode;
+}
--- demo/src/main/java/com/eaglebank/demo/model/User.java
+++ demo/src/main/java/com/eaglebank/demo/model/User.java
@@ -1,4 +1,30 @@
 package com.eaglebank.demo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    @Embedded
+    private Address address;
+    private String email;
+    private String phoneNumber;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<BankAccount> accounts;
+    private OffsetDateTime createdTimestamp;
+    private OffsetDateTime updatedTimestamp;
 }
