0bac7c20ab826b60f777c4f2a57232163ae5d929
bmkamal <55796617+bmkamal@users.noreply.github.com>
2025-06-09 23:08:28 +0200

    Added findByUserId method

--- demo/src/main/java/com/eaglebank/demo/repository/BankAccountRepository.java
+++ demo/src/main/java/com/eaglebank/demo/repository/BankAccountRepository.java
@@ -4,6 +4,10 @@ import com.eaglebank.demo.model.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+
+    List<BankAccount> findByUserId(String userId);
 }
