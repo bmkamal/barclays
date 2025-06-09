5ae089571e9563cea9de6c6b9efc721bbd34dd60
bmkamal <55796617+bmkamal@users.noreply.github.com>
2025-06-09 23:12:02 +0200

    Added findByEmail method

--- demo/src/main/java/com/eaglebank/demo/repository/UserRepository.java
+++ demo/src/main/java/com/eaglebank/demo/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package com.eaglebank.demo.repository;
 
 import com.eaglebank.demo.model.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
+    Optional<User> findByEmail(String email);
 }
